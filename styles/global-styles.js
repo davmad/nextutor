2c3b62827d1f06bc94a0c759f9d03b27b6112f7b
David Madar <davidm@humaneyes.com>
2018-11-04 17:12:31 +0200

    adding the global style

--- styles/global-styles.js
+++ styles/global-styles.js
@@ -1,10 +1,8 @@
-// import { injectGlobal } from "styled-components";
+import { injectGlobal } from 'styled-components'
 
-// const globStyle = injectGlobal`
-//   ul {
-//     margin: 0;
-//     padding: 0;
-//   }
-// `;
-
-// export default globStyle;
+export default () => injectGlobal`
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+`
